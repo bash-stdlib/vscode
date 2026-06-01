49b385c04e1af7749d9cc5f32ec2a3cdf5a2589b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-06-01 07:54:33 -0400

    enhanced linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,83 @@
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 import typescriptEslint from "typescript-eslint";
 
-export default [{
+export default [
+  {
     files: ["**/*.ts"],
-}, {
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
+      "@typescript-eslint": typescriptEslint.plugin,
+      import: importPlugin,
+      "unused-imports": unusedImports,
     },
 
     languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: typescriptEslint.parser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
+      curly: "warn",
+      eqeqeq: "warn",
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+          },
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+          groups: [
+            "external",
+            "builtin",
+            "sibling",
+            "parent",
+            "object",
+            "index",
+            "internal",
+            "type",
+          ],
+
+          "newlines-between": "never",
+
+          pathGroups: [
+            {
+              pattern: "@app/**",
+              group: "external",
+              position: "after",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+        },
+      ],
+      semi: "warn",
+      "unused-imports/no-unused-imports": "error",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: ["./*", "../*"],
+              message:
+                "Relative imports are not allowed. Please use absolute paths or aliases.",
+            },
+          ],
+        },
+      ],
+      "no-throw-literal": "warn",
     },
-}];
+  },
+];
